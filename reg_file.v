ffbfa58282af5216c19cd7046d6389c467b8d5ed
M10412034 <M10412034@yuntech.edu.tw>
2016-03-17 20:02:46 +0800

    將reg_file.v 改為參數化

--- reg_file.v
+++ reg_file.v
@@ -1,27 +1,23 @@
-module reg_file #(parameter WIDTH = 32,parameter DEPTH = 32)(
+module regfile #(parameter WIDTH = 16,parameter DEPTH = 8)(
 input clk,
-input rst,
 input we,
-input [5:0]select,
+input [$clog2(DEPTH)-1:0]read_reg1,read_reg2,
+input [$clog2(DEPTH)-1:0]write_reg,
 input [WIDTH-1:0]write_data,
-output [WIDTH-1:0] read_out
+output [WIDTH-1:0] read_out_1,
+output [WIDTH-1:0] read_out_2
 );
 integer i;
 parameter zero={(WIDTH){1'b0}};
-
 reg [WIDTH-1:0]file[DEPTH-1:0];
-
-assign read_out=file[select];
-
-always @(posedge clk,posedge rst)
+assign read_out_1=file[read_reg1];
+assign read_out_2=file[read_reg2];
+always @(posedge clk)
 begin
-	if(rst)
+	/*if(rst)
 	for(i=0;i<DEPTH;i=i+1)
 		file[i]=zero;
-	else if(we)
-		file[select]=write_data;
+	else */if(we)
+		file[write_reg]<=write_data;
 end
-
-
-
 endmodule
