44aea762bf195023fa74598258f26dc6eccaccb8
M10412034 <M10412034@yuntech.edu.tw>
2016-03-24 16:28:31 +0800

    加入 Mux4.v 4對1多工器

--- buffer.v
+++ buffer.v
@@ -1,19 +1,17 @@
-module buffer #(parameter WIDTH = 32)(
+module buffer #(parameter WIDTH = 32,reset=16'h3000)(
 input clk,
 input rst,
 input en,
 input[WIDTH-1:0] data_in,
 output reg[WIDTH-1:0] data_out
 );
-parameter zero={(WIDTH){1'b0}};
+
 
 
 always@(posedge clk)
 begin
 	if(rst)
-	data_out<=zero;
-	else if(clr)
-	data_out<={{(WIDTH){1'b0}}};
+	data_out<=reset;
 	else if(en)
 	data_out<=data_in;
 end
