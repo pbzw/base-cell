9ef76a16ae77d7a7f677b6a029f1c455a392b06c
M10412034 <M10412034@yuntech.edu.tw>
2016-04-13 19:14:51 +0800

    FIFO改為參數化

--- FIFO.v
+++ FIFO.v
@@ -1,31 +1,26 @@
-module FIFO(
+module FIFO#(parameter WIDTH = 32,parameter DEPTH = 64)(
 input clk,
 input rst,
 input put,
 input get,
-input [7:0]put_data,
+input [WIDTH-1:0]put_data,
 
 output full,
 output empty,
-output allmost_full,
-output allmost_empty,
-output [7:0]get_data
+output [WIDTH-1:0]get_data
 
 );
 
-reg [7:0]mem[0:511];
+reg [WIDTH-1:0]mem[0:DEPTH-1];
 
-wire [8:0]write_address;
-wire [8:0]read_address;
-wire [8:0]data_count;
+wire [$clog2(DEPTH)-1:0]write_address;
+wire [$clog2(DEPTH)-1:0]read_address;
 
 assign get_data=mem[read_address];
-assign full=(data_count>=9'd16);
-assign allmost_full=(data_count>=9'd15);
-assign empty=(data_count==9'd0);
-assign allmost_empty=(9'd1>=data_count);
+assign full=(write_address+{{$clog2(DEPTH)-1{1'b0}},1'b1}==read_address);
+assign empty=(write_address==read_address);
 
- up_counter_9bits write_address_count(
+ up_counter #(.WIDTH ($clog2(DEPTH)))  write_address_count(
 .clk(clk),
 .rst(rst),
 .en(put),
@@ -33,74 +28,18 @@ assign allmost_empty=(9'd1>=data_count);
 );
 
 
- up_counter_9bits read_address_count(
+ up_counter #(.WIDTH ($clog2(DEPTH))) read_address_count(
 .clk(clk),
 .rst(rst),
 .en(get),
 .count(read_address)
 );
 
-acc_counter_9bits data_counter(
-.clk(clk),
-.rst(rst),
-.add(put),
-.sub(get),
-.count(data_count)
-);
 
 always @(posedge clk)
 begin
 	if(put)
 	mem[write_address]=put_data;
-end
-
-endmodule
-
-module up_counter_9bits(
-input clk,
-input rst,
-input en,
-output reg[8:0] count
-);
-always@(posedge clk)
-begin
-	if(rst)
-	count<=9'd0;
-	else if(en)
-	count<=count+9'b1;
-end
-
-endmodule
-
-module acc_counter_9bits(
-input clk,
-input rst,
-input add,
-input sub,
-output reg[8:0] count
-);
-
-reg [8:0]count_new;
-
-always@(posedge clk)
-begin
-	if(rst)
-	count<=9'd0;
-	else 
-	count<=count_new;
-end
-
-always@(*)
-begin
-	case({add,sub})
-	2'b00:count_new<=count;
-	2'b01:count_new<=count-9'd1;
-	2'b10:count_new<=count+9'd1;
-	2'b11:count_new<=count;
-	default
-	count_new<=count;
-	endcase
-
 end
 
 endmodule
